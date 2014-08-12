6e0473c0b865db1bdad4f01af110a4238cc1753d
pastak <pasta0915@gmail.com>
2014-08-12 12:40:31 +0900

    fetch images as dataUrl with XHR and FileReader

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
 var host = "https://upload.gyazo.com/api/upload/easy_auth";
 var client_id = "df9edab530e84b4c56f9fcfa209aff1131c7d358a91d85cc20b9229e515d67dd";
+const INCREMENT_SIZE = 5;
+var notification = {
+  progress: 3,
+  limit: 30,
+  id: 'gyazo_notification_' + Date.now(),
+  newTabId: null,
+  progressIncrement: function(){
+    this.progress = Math.min(this.progress + INCREMENT_SIZE, this.limit);
+  }
+};
+
+function postToGyazo(dataUrl,tab){
+  $.ajax({
+    type: 'POST',
+    url:  host,
+    data: {
+      client_id: client_id,
+      url: dataUrl,
+      title: tab.title,
+      referer: tab.url
+    },
+    crossDomain: true})
+  .done(function(data) {
+      chrome.tabs.create({url:data.get_image_url, selected:false}, function(newTab){
+        notification.limit = 80;
+        notification.newTabId = newTab.id;
+        var handler = function (tabId, changeInfo) {
+          if (newTab.id == tabId && changeInfo.url) {
+            saveToClipboard(changeInfo.url);
+            chrome.tabs.onUpdated.removeListener(handler);
+            notification.newTabId = tabId;
+          }
+        };
+        chrome.tabs.onUpdated.addListener(handler);
+      });
+    })
+    .fail(function(XMLHttpRequest, textStatus, errorThrown) {
+      window.alert("Status: " + XMLHttpRequest.status + "\n Error: " + textStatus + "\n Message: " + errorThrown.message);
+    });
+}
 
 function onClickHandler(info, tab) {
 
   if (info.menuItemId == "gyazo_image") {
-    var image = new Image();
-    var canvas =  document.createElement('canvas');
-    var ctx = canvas.getContext("2d");
-    image.crossOrigin = "Anonymous";
-    const INCREMENT_SIZE = 5;
-    var notification = {
-      progress: 3,
-      limit: 30,
-      id: 'gyazo_notification_' + Date.now(),
-      newTabId: null,
-      progressIncrement: function(){
-        this.progress = Math.min(this.progress + INCREMENT_SIZE, this.limit);
+    var xhr = jQuery.ajaxSettings.xhr();
+    xhr.open('GET', info.srcUrl, true);
+    xhr.responseType = 'blob';
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4){
+        var blob = xhr.response;
+        var file_reader = new FileReader();
+        file_reader.onload = function(e){
+          postToGyazo(file_reader.result,tab);
+        }
+        file_reader.readAsDataURL(blob);
       }
     };
+    xhr.send();
     chrome.notifications.create(notification.id,{
       type: "progress",
       title: "Uploading to Gyazo",
@@ -44,39 +84,6 @@ function onClickHandler(info, tab) {
         })
       }
     },500);
-    image.onload = function(){
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.drawImage(image, 0, 0);
-      $.ajax({
-        type: 'POST',
-        url:  host,
-        data: {
-          client_id: client_id,
-          url: canvas.toDataURL(),
-          title: tab.title,
-          referer: tab.url
-        },
-        crossDomain: true})
-      .done(function(data) {
-          chrome.tabs.create({url:data.get_image_url, selected:false}, function(newTab){
-            notification.limit = 80;
-            notification.newTabId = newTab.id;
-            var handler = function (tabId, changeInfo) {
-              if (newTab.id == tabId && changeInfo.url) {
-                saveToClipboard(changeInfo.url);
-                chrome.tabs.onUpdated.removeListener(handler);
-                notification.newTabId = tabId;
-              }
-            };
-            chrome.tabs.onUpdated.addListener(handler);
-          });
-        })
-        .fail(function(XMLHttpRequest, textStatus, errorThrown) {
-          window.alert("Status: " + XMLHttpRequest.status + "\n Error: " + textStatus + "\n Message: " + errorThrown.message);
-        });
-    }
-    image.src = info.srcUrl;
   }
 }
 
