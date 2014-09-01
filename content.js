66b604e6655e703a23cb34612b30a2949d23dc54
pastak <pasta0915@gmail.com>
2014-09-01 15:53:42 +0900

    if less than 1px image, doesn't post

--- content.js
+++ content.js
@@ -63,6 +63,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
         window.addEventListener('contextmenu', cancelGyazo);
         data.w = Math.abs(e.clientX - startX);
         data.h = Math.abs(e.clientY - startY);
+        if(data.h < 1 || data.w < 1){
+          document.body.removeChild(layer);
+          return false;
+        }
         data.x = Math.min(e.clientX, startX);
         data.y = Math.min(e.clientY, startY);
         data.t = document.title;
