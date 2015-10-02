6733d0993939a3285796a60e1f24a640833741b1
drobune <s.yuichi227@gmail.com>
2015-10-02 16:41:38 +0900

    Tweak regex

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function postToGyazo (data) {
       title: data.title,
       referer_url: data.url,
       scale: data.scale || '',
-      desc: data.desc.replace(/^\s+|\s+$/g, '').replace(/^[\n\r]/, '') || ''
+      desc: data.desc.replace(/\t/, ' ').replace(/(^\s+| +$)/gm, '') || ''
     },
     crossDomain: true
   })
