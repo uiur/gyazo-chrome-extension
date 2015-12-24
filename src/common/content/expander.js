5ec39ee97f7a6fd828e71f420edd328e1829d57f
Kazato Sugimoto <uiureo@gmail.com>
2015-12-24 19:35:37 +0900

    Extract functions

--- src/common/content/expander.js
+++ src/common/content/expander.js
@@ -1,4 +1,4 @@
-const gyazoIdFromUrl = function (str) {
+function gyazoIdFromUrl (str) {
   let parsedUrl = ''
   try {
     parsedUrl = new window.URL(str)
@@ -9,6 +9,26 @@ const gyazoIdFromUrl = function (str) {
   if ((/^(.+\.)?gyazo\.com$/).test(parsedUrl.host) && (/^\/[0-9a-f]+$/).test(parsedUrl.pathname)) {
     return parsedUrl.pathname.slice(1)
   }
+}
+
+function fetchImage (url, callback) {
+  chrome.runtime.sendMessage(chrome.runtime.id, {
+    action: 'gyazoGetImageBlob',
+    gyazoUrl: url
+  }, (response) => {
+    const xhr = new window.XMLHttpRequest()
+    xhr.open('GET', response.imageBlobUrl, true)
+    xhr.responseType = 'arraybuffer'
+    xhr.onload = () => {
+      const blob = new window.Blob([xhr.response], { type: 'image/png' })
+
+      callback(null, blob)
+    }
+    xhr.onerror = (e) => {
+      callback(e)
+    }
+    xhr.send()
+  })
 }
 
 const onNewElement = function (cb) {
@@ -22,42 +42,37 @@ const onNewElement = function (cb) {
   }, 1000)
 }
 
-const isEnabledHost = function (_url) {
+function isEnabledHost (_url) {
   let parsedUrl = ''
   try {
     parsedUrl = new window.URL(_url)
   } catch (e) {
     return false
   }
-  return parsedUrl.host.match(/^github\.com$/) ||
-    parsedUrl.host.match(/^(.+\.)?zendesk\.com$/) ||
-    parsedUrl.host.match(/chatwork\.com$/)
+
+  const hosts = [
+    /^github\.com$/,
+    /^(.+\.)?zendesk\.com$/,
+    /chatwork\.com$/
+  ]
+
+  return hosts.some((host) => host.test(parsedUrl.host))
 }
 
-if (isEnabledHost(location.href)) {
+if (isEnabledHost(window.location.href)) {
   onNewElement((el) => {
     const href = el.getAttribute('href')
 
     const isGyazoUrl = !!gyazoIdFromUrl(href)
     const hasChildren = el.children.length > 0
     if (isGyazoUrl && !hasChildren) {
-      chrome.runtime.sendMessage(chrome.runtime.id, {
-        action: 'gyazoGetImageBlob',
-        gyazoUrl: href
-      }, (response) => {
-        const xhr = new window.XMLHttpRequest()
-        xhr.open('GET', response.imageBlobUrl, true)
-        xhr.responseType = 'arraybuffer'
-        xhr.onload = () => {
-          const blob = new window.Blob([xhr.response], {type: 'image/png'})
-          el.insertAdjacentHTML('afterend',
-          `<p>
-            <a href=${ href } target='_blank' data-gyazo-id='checked'>
-              <img src=${ window.URL.createObjectURL(blob) } style='max-width: 100%;' />
-            </a>
-          </p>`)
-        }
-        xhr.send()
+      fetchImage(href, (e, blob) => {
+        el.insertAdjacentHTML('afterend',
+        `<p>
+          <a href=${ href } target='_blank' data-gyazo-id='checked'>
+            <img src=${ window.URL.createObjectURL(blob) } style='max-width: 100%;' />
+          </a>
+        </p>`)
       })
     }
   })
